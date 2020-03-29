b2e6dfc8c9d34385265b3d2f04471bebd37ef5b2
Nick An <anning0322@gmail.com>
2020-03-29 14:23:56 +1100

    set buffer to 0

--- index.rb
+++ index.rb
@@ -2,7 +2,7 @@ require 'r_socks'
 
 server = RSocks::TcpServer.new
 server.config.auth_method = :password
-server.config.proxy_buffer_size = 30 * 1024 * 1024
+server.config.proxy_buffer_size = 0
 server.config.proxy_type = :http
 
 server.config.enable_ssl = true
