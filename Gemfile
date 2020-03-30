356c42942b88c646abe433f11af2d0d89a69c5ca
Nick An <anning0322@gmail.com>
2020-03-30 19:09:32 +1100

    update to 0.2.4

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source "https://rubygems.org"
 
-gem 'r_socks', '~> 0.2.3'
+gem 'r_socks', '~> 0.2.4'
--- index.rb
+++ index.rb
@@ -2,12 +2,17 @@ require 'r_socks'
 
 server = RSocks::TcpServer.new
 server.config.auth_method = :password
-server.config.proxy_buffer_size = 0
+server.config.proxy_buffer_size = 10 * 1024 * 1024
 server.config.proxy_type = :http
 
-server.config.enable_ssl = true
+# if true then you need attach cert and private key
+server.config.enable_ssl = false
+# server.config.ssl_private_key = './server_key.txt'
+# server.config.ssl_cert = './server_cert.txt'
 
-server.config.ssl_private_key = './server_key.txt'
-server.config.ssl_cert = './server_cert.txt'
+# start multi process
+server.config.instances = 2
+
+
 
 server.run!
